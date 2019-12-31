5b1ab63d00ec0efdeef04eeed0d86d564b5858f0
rastrainj <rastrain@outlook.es>
2019-12-31 10:30:16 +0100

    Updates

---
+++ precache-manifest.840b54f770e26888d13f30347659db5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86578aded0d7e348e3ea354c0b4725cd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "69c3067b50bc00508342",
+    "url": "/monsters-rolodex/static/css/main.cb97f3b3.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69c3067b50bc00508342",
+    "url": "/monsters-rolodex/static/js/main.ffda6082.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
